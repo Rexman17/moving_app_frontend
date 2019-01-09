4e5be1af75a8aeb1c79ebf51fca83abd8fba4631
Rexman17 <a.stone1794@gmail.com>
2019-01-09 11:56:50 -0500

    Imported Google Materialize CSS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+// Create the Redux store:
+// const store = createStore(rootReducer)
+// Wrap App with Provider
+ReactDOM.render(
+
+    <App />
+  ,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
