a064cde737e7eab5f91d3f7c6d546cf0e6dc5810
Rexman17 <a.stone1794@gmail.com>
2019-01-13 09:28:27 -0500

    successful POST request for boxes

--- src/components/BoxList.js
+++ src/components/BoxList.js
@@ -29,7 +29,7 @@ class BoxList extends React.Component {
 }
 
 const mapStateToProps = state => {
-  console.log("STATE IS", state);
+  // console.log("STATE IS", state);
   return {
     move: state.move,
     boxes: state.boxes,
