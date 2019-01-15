499ec9dfd68e43ec910247bc30d4d42268c27f5c
Rexman17 <a.stone1794@gmail.com>
2019-01-15 15:44:51 -0500

    Added composeWithDevTools extension

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer'
@@ -11,12 +11,8 @@ import thunk from 'redux-thunk';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 // Create the Redux store:
-const store = createStore(rootReducer, applyMiddleware(thunk))
-// console.log("REDUX STORE IS", store);
-// console.log("==================");
-// console.log("get state function", store.getState());
-// console.log('%c get state function','background: blue; color: white; display: block;', store.getState());
-// Wrap App with Provider
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,20 +2,35 @@
  // ONCE I HAVE LOG IN WILL NOT HARD CODE USERID
 
 const initialState = {
-  username: '',
-  user_id: 1
+  user: null,
+  loggedIn: false,
+  authenticatingUser: false,
+  failedLogin: false,
 }
 
 const userReducer = (state=initialState, action) => {
 
   switch (action.type) {
 
+    case 'SET_CURRENT_USER':
+     //action.payload { username: 'Chandler Bing', bio: 'my user bio', avatar: 'some image url' }
+     return { ...state, user: action.payload, loggedIn: true, authenticatingUser: false }
+   case 'AUTHENTICATING_USER': //tells the app we're fetching
+     return { ...state, authenticatingUser: true }
+   case 'AUTHENTICATED_USER':
+     return { ...state, authenticatingUser: false }
+   case 'FAILED_LOGIN': //for error handling
+     return {
+       ...state,
+       failedLogin: true,
+       error: action.payload,
+       authenticatingUser: false
+     }
+
     default:
       return state;
 
   }
-
-
 
 }
 
