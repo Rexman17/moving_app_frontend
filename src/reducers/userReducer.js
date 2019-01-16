63f3148ebdd59df84eca45d65b213ca726f2b524
Rexman17 <a.stone1794@gmail.com>
2019-01-15 19:53:44 -0500

    added withAuth HOC and wrapped MoveContainer, still cannot see anything once i click log in

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -15,10 +15,13 @@ const userReducer = (state=initialState, action) => {
     case 'SET_CURRENT_USER':
      //action.payload { username: 'Chandler Bing', bio: 'my user bio', avatar: 'some image url' }
      return { ...state, user: action.payload, loggedIn: true, authenticatingUser: false }
+
    case 'AUTHENTICATING_USER': //tells the app we're fetching
      return { ...state, authenticatingUser: true }
+
    case 'AUTHENTICATED_USER':
      return { ...state, authenticatingUser: false }
+
    case 'FAILED_LOGIN': //for error handling
      return {
        ...state,
